dc8e9b9ad8b429cf51cb03b84892ce33d4f0cfdc
chaemin58 <sulv0309@naver.com>
2026-09-21 01:12:01 +0900

    fix: 하드코딩 되어 있는 값을 지우고 값을 파라미터로 받아 유동적으로 렌더링할 수 있도록 변경

--- lib/widget/profile/stat_card.dart
+++ lib/widget/profile/stat_card.dart
@@ -2,45 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:movielog/theme/app_colors.dart';
 
 class StatCard extends StatelessWidget {
-  const StatCard({super.key});
+  const StatCard({super.key, required this.label, required this.value});
+
+  final String label;
+  final String value;
 
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    return Scaffold(
-      body: Center(
-        child: Column(
-          spacing: 5,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 38),
-              decoration: BoxDecoration(
-                color: AppColors.surfaceLow,
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(color: AppColors.primary200),
-              ),
-              child: Column(
-                spacing: 8,
-                children: [
-                  Text(
-                    '본 영화',
-                    style: textTheme.labelSmall?.copyWith(
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  Text(
-                    '342',
-                    style: textTheme.titleLarge?.copyWith(
-                      fontSize: 22,
-                      color: AppColors.primary600,
-                    ),
-                  ),
-                ],
-              ),
+    return Center(
+      child: Column(
+        spacing: 5,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 38),
+            decoration: BoxDecoration(
+              color: AppColors.surfaceLow,
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(color: AppColors.primary200),
             ),
-          ],
-        ),
+            child: Column(
+              spacing: 8,
+              children: [
+                Text(
+                  label,
+                  style: textTheme.labelSmall?.copyWith(
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  value,
+                  style: textTheme.titleLarge?.copyWith(
+                    fontSize: 22,
+                    color: AppColors.primary600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
